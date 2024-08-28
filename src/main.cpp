a15df78161bb03424dd3dbde2b332d2bf0d7f5d7
Logan Ruddick <itsky5000@gmail.com>
2024-08-28 14:35:27 -0700

    Arduino Giga Test Main

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,31 @@
 #include <Arduino.h>
+#include <SerialRPC.h>
+#include <RPC.h>
 
 // put function declarations here:
-int myFunction(int, int);
+#if !defined(GIGA_R1_M4) && !defined(GIGA_R1_M7)
+  #error "This sketch is for the Arduino Giga R1 only."
+#endif
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  while (!Serial) continue;
+  if (!SerialRPC.begin()) Serial.println("Failed to start SerialRPC");
+
+  if (RPC.cpu_id() == CM7_CPUID) {
+    Serial.println("This is the CM7 core");
+  }
+  else if (RPC.cpu_id() == CM4_CPUID) {
+    SerialRPC.println("This is the CM4 core");
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  if (RPC.cpu_id() == CM7_CPUID) {
+    Serial.println("CM7 Core: Loop");
+  }
+  else if (RPC.cpu_id() == CM4_CPUID) {
+    SerialRPC.println("CM4 Core: Loop");
+  }
+  delay(1000);
 }
